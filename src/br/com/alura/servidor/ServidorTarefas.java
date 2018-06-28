dc786147267168a493d75c1aeb7393c3ba9dec83
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-28 16:36:46 -0300

    Criando Um Pool de threads para reaproveitar threads já abertas para novos processamentos

--- src/br/com/alura/servidor/ServidorTarefas.java
+++ src/br/com/alura/servidor/ServidorTarefas.java
@@ -3,6 +3,9 @@ package br.com.alura.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
 
@@ -11,15 +14,15 @@ public class ServidorTarefas {
 		System.out.println("------- Iniciando Servidor ----");
 		ServerSocket servidor = new ServerSocket(12345);
 		
-		
+		//Aproveitando Threads em um pool de threads ou seja o sistema utiliza threads já abertas
+		ExecutorService threadPool = Executors.newFixedThreadPool(2);
 		
 		while(true) {
 			Socket socket =  servidor.accept();
 			System.out.println("Aceitando novo cliente na porta "+ socket.getPort());
 			
 			DistribuirTarefas distribuirTarefas = new DistribuirTarefas(socket);
-			Thread threadCliente = new Thread(distribuirTarefas);
-			threadCliente.start();
+			threadPool.execute(distribuirTarefas);
 			
 			
 		}
