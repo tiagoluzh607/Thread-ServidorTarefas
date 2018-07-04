4e96ce615dfd35950ad8546b767b6e34a8a2a28f
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-04 16:59:57 -0300

    Criando um classe de Teste para vermos a diferenca com variavel normal, volatile e atomic

---
+++ src/br/com/alura/testevolatile/ServidorDeTeste.java
@@ -0,0 +1,44 @@
+package br.com.alura.testevolatile;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ServidorDeTeste {
+
+	//private boolean estaRodando = false; variavel não atomica
+	private volatile boolean estaRodando = false; //Uso do volatile para threads acessarem direto memória principal
+	//private AtomicBoolean estaRodando(); //mesmo com o volatile teriamos que syncronizar o acesso a variavel, por isso o melhor é usar a classe atomica, que faz as duas coisas acessa direto a memoria e syncroniza o acesso a esta propriedade de forma implicita
+	// para usar o Atomic para mudar a variavel é atraves do get e set exemplo: estaRodando.set(false); estaRodando.set(true);
+    public static void main(String[] args) throws InterruptedException {
+        ServidorDeTeste servidor = new ServidorDeTeste();
+        servidor.rodar();
+        servidor.alterandoAtributo();
+    }
+
+    private void rodar() {
+        new Thread(new Runnable() {
+
+            public void run() {
+                System.out.println("Servidor começando, estaRodando = " + estaRodando );
+
+                while(!estaRodando) {}
+
+                System.out.println("Servidor rodando, estaRodando = " + estaRodando );
+
+                while(estaRodando) {}
+
+                System.out.println("Servidor terminando, estaRodando = " + estaRodando );
+            }
+        }).start();
+    }
+
+    private void alterandoAtributo() throws InterruptedException {
+        Thread.sleep(5000);
+        System.out.println("Main alterando estaRodando = true");
+        estaRodando = true;
+
+        Thread.sleep(5000);
+        System.out.println("Main alterando estaRodando = false");
+        estaRodando = false;        
+    }
+}
+
