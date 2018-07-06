80c5d06368a87ce2aabcabe9629050a2799710cb
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-07-06 09:21:30 -0300

    TESTE - fazendo uma fila, syncronizada para uso de um ou mais threads

--- src/br/com/alura/testevolatile/TesteFila.java
+++ src/br/com/alura/testevolatile/TesteFila.java
@@ -2,20 +2,22 @@ package br.com.alura.testevolatile;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class TesteFila {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		Queue<String> fila = new LinkedList<>();
+		BlockingQueue<String> fila = new ArrayBlockingQueue<>(3);
 
-		fila.offer("c1");
-		fila.offer("c2");
-		fila.offer("c3");
+		fila.put("c1"); // o put adiciona um elemento na lista porem agora usando BlockingQueue se a fila estiver cheia ele trava o thread esperando liberar um espaço para colocar o elemento
+		fila.put("c2");
+		fila.put("c3");
 		
-		System.out.println(fila.poll());
-		System.out.println(fila.poll());
-		System.out.println(fila.poll());
+		System.out.println(fila.take()); //o Take tira um elemento da lista porem agora usando BlockingQueue se a fila nao tiver mais elementos ele trava o thread aguardando que um elemento entre para tirar
+		System.out.println(fila.take());
+		System.out.println(fila.take());
 		
 		System.out.println(fila.size());
 	}
